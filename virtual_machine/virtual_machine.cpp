fb4a6ce6db6ad0d93e52f8affdb7e17c5f945200
Ilya <zaxsad109@gmail.com>
2024-12-17 00:12:12 +0300

    Revert "/"
    This reverts commit d4ec5aa3d2aae56fb11ac9f6c5d244238c00a1cc.

--- virtual_machine/virtual_machine.cpp
+++ virtual_machine/virtual_machine.cpp
@@ -7,18 +7,12 @@ using namespace std;
 int main() {
     vm vm;
 
-    auto res = vm.exec(R"(var number = 5;
 
-func factorial(n) {
-    if (n == 0) {
-       return 1;
-    } else {
-       return n * factorial(n - 1);
+    int i = 0;
+    for (; !(i >= 5); i = i + 1) {
+        // No operation inside the loop
     }
-}
+    std::cout << i;
 
-var fact = factorial(number);
-fact;)");
-
-    int x=10;
+//     ForLoopVariableScope  ForLoopVariableScopeCheck
 }
